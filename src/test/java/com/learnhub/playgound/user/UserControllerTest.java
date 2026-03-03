ac03d17e0dff45b6a9a73c8156e4ed2a92a2ddc8
chee_suttiporn <158833717+6451000985@users.noreply.github.com>
2026-03-04 00:57:35 +0700

    First Integration Testing  userController

---
+++ src/test/java/com/learnhub/playgound/user/UserControllerTest.java
@@ -0,0 +1,88 @@
+package com.learnhub.playgound.user;
+
+
+import com.learnhub.playgound.config.JwtService;
+import com.learnhub.playgound.user.controller.UserController;
+import com.learnhub.playgound.user.domain.User;
+import com.learnhub.playgound.user.dto.UserResponse;
+import com.learnhub.playgound.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+@WebMvcTest(UserController.class)
+ public class UserControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @MockitoBean
+    private UserService userService;
+
+    @MockitoBean
+    private JwtService jwtService;
+
+    @MockitoBean
+    private UserDetailsService userDetailsService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    @WithMockUser
+    void WhenGetUserById_ThenSuccessfully() throws Exception{
+
+        Long id =1L;
+
+        LocalDateTime expectedTime = LocalDateTime.of(2025, 2, 14, 17, 0);
+
+        User savedUser = new User();
+        savedUser.setId(1L);
+        savedUser.setEmail("test@mail.com");
+        savedUser.setPasswordHash("hashed");
+        savedUser.setFirstName("John");
+        savedUser.setLastName("Doe");
+        savedUser.setAvatarUrl("testavatarurl.com");
+        savedUser.setActive(true);
+        savedUser.setVerified(false);
+        savedUser.setLastLogin(expectedTime);
+        savedUser.setCreatedAt(expectedTime);
+        savedUser.setUpdatedAt(expectedTime);
+
+        when(userService.getUserById(id))
+                .thenReturn(UserResponse.fromEntity(savedUser));
+        mockMvc.perform(
+                get(String.format("/api/users/%d",1))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.email").value("test@mail.com"))
+                .andExpect(jsonPath("$.firstName").value("John"))
+                .andExpect(jsonPath("$.lastName").value("Doe"))
+                .andExpect(jsonPath("$.avatarUrl").value("testavatarurl.com"))
+                .andExpect(jsonPath("$.isActive").value("true"))
+                .andExpect(jsonPath("$.isVerified").value("false"));
+
+    }
+}
